1423f4f79c2a6c76c04ead917d963b85aeb921b7
David Bohman <david.bohman.dev@gmail.com>
2024-08-19 02:33:22 -0600

    model: add Game class w/ core attr

--- models.py
+++ models.py
@@ -4,6 +4,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Game(db.Model):
+    """A game for which Nexus hosts mods."""
+
+    __tablename__ = 'games'
+
+    # ID in this model will match Nexus game ID
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    # slug of game name used by Nexus for game page url
+    domain_name = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    name = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f'<Game #{self.id}: "{self.name}">'
+
+
 class User(db.Model):
     """User in the system."""
 
@@ -48,3 +74,16 @@ class User(db.Model):
         default=True,
         nullable=False
     )
+
+
+
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    Call this in the Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
