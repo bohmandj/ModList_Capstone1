e136e66e5776f72685668cc1568cede2d11b49b2
David Bohman <david.bohman.dev@gmail.com>
2024-09-13 17:24:48 -0600

    feat: add get_mods...() for mod types from Nexus api

--- nexus_api.py
+++ nexus_api.py
@@ -19,7 +19,8 @@ def get_all_games_nxs(include_unapproved=False):
     "mods": int, "categories": [ {"category_id": int, 
     "name": str, "parent_category": bool} ] }
 
-    Optionally can also include unapproved games. ( include_unapproved=True )"""
+    Optionally can also include unapproved games. ( include_unapproved=True )
+    """
 
     url = f'{base_url}v1/games.json'
 
@@ -28,14 +29,50 @@ def get_all_games_nxs(include_unapproved=False):
 
     try:
         res = requests.get(url, params=include_unapproved, headers=headers)
-        nexus_games = res.json()
 
-    except Exception as e:
-        print("Failed to retrieve API data", res.status_code)
+    except requests.exceptions.RequestException as e:
+        print("get_all_games_nxs() Failed to retrieve API data: ", res.status_code)
         print("Error: ", e)
-        return e
+        raise
+
+    nexus_games = res.json()
 
     return nexus_games
             
 
-         
+def get_mods_of_type_nxs(game, request_type):
+    """Nexus API call.
+    
+    Returns list of most recently added mods for provided 
+    game on Nexus if successful, or Exception details.
+
+    Valid request_type inputs: 
+        'latest_added', 'latest_updated', 'trending'
+    
+    Example returned mod data: 
+        { 'name': str, 'summary': str, 'description': str, 'picture_url': str, 
+        'mod_downloads': num, 'mod_unique_downloads': num, 'uid': num, 
+        'mod_id': num, 'game_id': num, 'allow_rating': true, 'domain_name': str, 
+        'category_id': num, 'version': str(of int),	'endorsement_count': num, 
+        'created_timestamp': num, 'created_time': datetime (str of int), 
+        'updated_timestamp': num, 'updated_time': datetime (str of int), 
+        'author': str, 'uploaded_by': str, 'uploaded_users_profile_url': str, 
+        'contains_adult_content': bool, 'status': str - 'published' / 'not_published', 
+        'available': bool, 'user': { 'member_id': num, 'member_group_id': num, 'name': str }, 
+        'endorsement': null }
+    """
+
+    url = f'{base_url}v1/games/{game.domain_name}/mods/{request_type}.json'
+
+    try:
+        res = requests.get(url, headers=headers)
+        print(f'NEXUS API RESPONSE: ', res)
+
+    except requests.exceptions.RequestException as e:
+        print("get_mods_of_type_nxs() Failed to retrieve API data: ", res.status_code)
+        print("Error: ", e)
+        raise
+
+    nexus_mods = res.json()
+
+    return nexus_mods
