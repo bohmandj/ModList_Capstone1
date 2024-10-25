4f0d11a30585245b3e77cfe3f3f9bd255b74aacd
David Bohman <david.bohman.dev@gmail.com>
2024-10-25 14:27:58 -0600

    feat: add scroll up button w/ js evt listeners

--- static/js/base.js
+++ static/js/base.js
@@ -1,12 +1,29 @@
 
-// show loading screen shown after clicking buttons w/ class 'show-loading'
 document.addEventListener('DOMContentLoaded', function () {
     const showLoadingButtons = document.querySelectorAll('.show-loading');
     const loadingContainer = document.getElementById('loading-container');
+    const scrollToTopButton = document.getElementById('scrollToTop');
 
+    // Show loading screen on button click for route that could take time
     showLoadingButtons.forEach(button => {
         button.addEventListener('click', function () {
             loadingContainer.style.display = 'flex';
         });
+    });
+
+    // Show/hide scroll-to-top button based on scroll position
+    window.addEventListener('scroll', function () {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            if (window.innerWidth >= 768) { // Don't show scroll up btn on phones
+                scrollToTopButton.style.display = "block";
+            }
+        } else {
+            scrollToTopButton.style.display = "none";
+        }
+    });
+
+    // Scroll to top when the button is clicked
+    scrollToTopButton.addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
