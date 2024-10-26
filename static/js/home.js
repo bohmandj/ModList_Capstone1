52a1e896d205a5e2674ac107983e3300e2238938
David Bohman <david.bohman.dev@gmail.com>
2024-10-26 05:04:41 -0600

    feat: add 'more games' btn on homepage to render more

--- static/js/home.js
+++ static/js/home.js
@@ -35,4 +35,35 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
+    let popularGameCardIndex = 10; // 10 most popular games' cards already shown
+
+    document.getElementById('load-more').addEventListener('click', function () {
+        // Btn functionality to display more popular games below the starting 
+        // 10 games every time the #load-more btn is clicked
+
+        const gameCardSection = document.querySelector('.game-section');
+
+        for (let i = 0; i < 10 && popularGameCardIndex < games.length; i++, popularGameCardIndex++) {
+            const game = games[popularGameCardIndex];
+
+            const newGameCard = document.createElement('li');
+            newGameCard.classList.add('game-card');
+            newGameCard.innerHTML = `
+            <a class="game-img" href="/games/${game.domain_name}" style="background-image: url(https://staticdelivery.nexusmods.com/Images/games/4_3/tile_${game.id}.jpg);" alt="${game.name} cover image" title="Open ${game.name} page on ModList">
+            </a>
+            <a class="nexus-btn-container" href="https://www.nexusmods.com/${game.domain_name}" target="_blank" rel="noopener noreferrer" title="Open ${game.name} on Nexus to search all mods in a new tab.">
+                <img class="nexus-btn" src="https://raw.githubusercontent.com/github/explore/781dbc058383a2ee8259ebbab057292f16172d5e/topics/nexus-mods/nexus-mods.png">
+            </a>
+            <a class="game-title" href="/games/${game.domain_name}" title="Open ${game.name} page on ModList">
+                <span>${game.name}</span>
+            </a>
+        `;
+            gameCardSection.appendChild(newGameCard);
+        }
+
+        if (popularGameCardIndex >= games.length) {
+            this.style.display = 'none';
+        }
+    });
 });
